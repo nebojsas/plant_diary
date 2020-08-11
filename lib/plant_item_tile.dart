bf9247e8158544da02307a099280fdb5bd86c43f
Nebojsa Sokolovic <nebojsa.sokolovic@gmail.com>
2020-08-11 13:06:04 +0200

    Added plant details page with sliver app bar

--- lib/plant_item_tile.dart
+++ lib/plant_item_tile.dart
@@ -10,7 +10,8 @@ class PlantItemTile extends StatelessWidget {
 
   const PlantItemTile({
     Key key,
-    this.plant, this.buildContext,
+    this.plant,
+    this.buildContext,
   }) : super(key: key);
 
   @override
@@ -22,10 +23,21 @@ class PlantItemTile extends StatelessWidget {
           leading: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Icon(
-                Icons.local_florist,
-                color: Colors.green,
-              ),
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: plant.imageUrl != null
+                      ? Image.network(
+                          plant.imageUrl,
+                          width: 48,
+                          height: 48,
+                          fit: BoxFit.fitWidth,
+                        )
+                      : Image.asset(
+                          'assets/default_plant.png',
+                          width: 48,
+                          height: 48,
+                          fit: BoxFit.fitWidth,
+                        )),
             ],
           ),
           title: Text(plant.name),
@@ -45,7 +57,70 @@ class PlantItemTile extends StatelessWidget {
               ),
             ],
           ),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) => PlantDetailsPage(
+                          plant: plant,
+                        )));
+          },
         ),
+      ),
+    );
+  }
+}
+
+class PlantDetailsPage extends StatefulWidget {
+  final Plant plant;
+
+  const PlantDetailsPage({
+    Key key,
+    this.plant,
+  }) : super(key: key);
+
+  @override
+  _PlantDetailsPageState createState() => _PlantDetailsPageState();
+}
+
+class _PlantDetailsPageState extends State<PlantDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   // Here we take the value from the MyHomePage object that was created by
+      //   // the App.build method, and use it to set our appbar title.
+      //   title: Text(widget.plant.name),
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300.0,
+            pinned: true,
+            floating: true,
+            snap: true,
+            flexibleSpace: FlexibleSpaceBar(
+                title: Text(widget.plant.name,
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+                background: widget.plant.imageUrl != null
+                    ? Image.network(
+                        widget.plant.imageUrl,
+                        fit: BoxFit.cover,
+                      )
+                    : Image.asset(
+                        'assets/default_plant.png',
+                        fit: BoxFit.contain,
+                      )),
+          ),
+          SliverFillRemaining(
+            child: Center(
+              child: Text(
+                  '${widget.plant.name} is a type of ${widget.plant.species}'),
+            ),
+          ),
+        ],
       ),
     );
   }
